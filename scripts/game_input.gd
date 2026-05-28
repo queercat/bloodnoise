cf766051c8fb1aa9062515e115ad26fff6160d7d
May Tusek <tusek.may@gmail.com>
2026-05-28 10:43:02 -0700

    blood

--- scripts/game_input.gd
+++ scripts/game_input.gd
@@ -3,6 +3,7 @@ extends Node
 @export var sound_sphere_prefab: PackedScene
 @export var body: CharacterBody3D
 @export var sound_cooldown : float = 1.0 
+@export var animated_sprite: AnimatedSprite3D
 var sound_cooldown_timer : float = 0
 
 func spawn_sound_sphere():
@@ -17,8 +18,11 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("primary_action") and sound_cooldown_timer <= 0:
+		animated_sprite.stop()
+		animated_sprite.play("shake")
 		sound_cooldown_timer = sound_cooldown
 		spawn_sound_sphere()
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
