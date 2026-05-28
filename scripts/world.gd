307fdaba3b0824eaa135d14dd25fd11968c08649
May Tusek <tusek.may@gmail.com>
2026-05-27 21:35:03 -0700

    blood

--- scripts/world.gd
+++ scripts/world.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(player)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
