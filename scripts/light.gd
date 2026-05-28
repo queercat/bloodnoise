fb058be137942ffdccdb1ca9f1c9ef1bc75eae44
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2026-05-27 22:24:04 -0700

    bluuddd

--- scripts/light.gd
+++ scripts/light.gd
@@ -1,12 +1,23 @@
 extends Node3D
 
 @export var light: OmniLight3D
+@export var light_on : bool = false
+@export var turn_off_after : float = 2.0
 
+var tween
+var decay_counter = 0;
 # when sound sphere hits me, turn on!
 
 func turn_on_light():
 	light.light_energy = 1
-
+	light_on = true
+	
+func turn_off_light(in_secs):
+	tween = 	get_tree().root.create_tween()
+	tween.tween_property(light, "light_energy", 0, in_secs)
+	await tween.finished
+	light_on = false
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,8 +26,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_area_3d_area_entered(area: Area3D) -> void:
-	turn_on_light()
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	turn_on_light()
+func _on_area_3d_area_entered(body: Node3D) -> void:
+	if not light_on:
+		turn_on_light()
+		turn_off_light(turn_off_after)
+	else:
+		tween.kill()
+		turn_on_light()
+		turn_off_light(turn_off_after)	
