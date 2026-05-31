aeb894754f773d65122abf9f3e4a31ecf23e16dc
May Tusek <tusek.may@gmail.com>
2026-05-31 05:44:42 -0700

    blood
    blood
    
    blood

---
+++ scripts/bell.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+@onready var stream: AudioStreamPlayer3D = $"AudioStreamPlayer3D"
+@export var time_positions: Array[float] = []
+var burned_times: Array[int] = []
+var tolerance = .01
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func get_playable_positions():
+	return time_positions.filter(func (v): return not burned_times.has(v))
+
+func bong():
+	GameManager.spawn_sound_sphere(global_position, 100, Color.PURPLE, 500)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if stream.playing:
+		var current_time = stream.get_playback_position()
+		var positions = get_playable_positions()
+		
+		for time_marker in positions:
+			if time_marker >= current_time - tolerance and time_marker <= current_time + tolerance:
+				bong()
+				burned_times.append(time_marker)
--- scripts/sound_sphere.gd
+++ scripts/sound_sphere.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var growth_speed: float = 2000000
-@export var maximum_radius: float = 50
+@export var max_radius: float = 50
 @export var color: Color = Color.WHITE
 var radius = 0
 
@@ -13,6 +13,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:	
 	scale += Vector3.ONE * growth_speed * delta
 	radius = scale.x
-	if radius >= maximum_radius:
+	if radius >= max_radius:
 		GameManager.delete_sound_sphere(self)
 		self.queue_free()
