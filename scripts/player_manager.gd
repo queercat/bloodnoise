7b508c1b82f27d5d48d514fbf3efc83b6d30e897
May Tusek <tusek.may@gmail.com>
2026-06-01 00:09:28 -0700

    blood

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -6,6 +6,7 @@ class_name PlayerManager
 @export var body: CharacterBody3D
 @export var sound_cooldown : float = 1.0 
 @export var animated_sprite: AnimatedSprite3D
+@export var world_mesh: MeshInstance3D
 
 var sound_cooldown_timer : float = 0
 var interactable_queue: Array[Interactable] = []
@@ -24,11 +25,12 @@ func pop_interactable():
 		interactable_queue.front().show_interaction()
 
 func spawn_sound_sphere():
-	GameManager.spawn_sound_sphere(body.global_position, 25, Color.RED)
+	GameManager.spawn_sound_sphere(body.global_position, 25, Color.RED, 50)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.player_manager = self
+	world_mesh.show()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("primary_action") and sound_cooldown_timer <= 0:
