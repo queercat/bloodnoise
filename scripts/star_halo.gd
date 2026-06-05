bac10338c56867f970e739967c24b9c0fe502d11
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2026-06-04 20:50:43 -0700

    add hand bell, star, new key

---
+++ scripts/star_halo.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+# Rotation in radians
+@export var sprite_rotation : float = 0.0
+# Upright position (still rotateable)
+@export var y_billboard = false
+
+var anim_player : AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim_player = $"./AnimationPlayer"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	anim_player.play("idle")
+	# Get camera coordinates
+	var cam : Camera3D = get_viewport().get_camera_3d()
+	var look_pos : Vector3 = cam.global_position
+	
+	# If billboard should be upright, look at camera, but at the height of the object itself
+	if y_billboard:
+		look_pos.y = global_position.y
+	# First look directly at the camera, then rotate around own axis
+	# Note: as an up axis here is camera's up, but one might want to use Vector3.UP instead
+	look_at(look_pos, cam.global_transform.basis.y)
+	rotate_object_local(Vector3.UP, sprite_rotation)
